e94d28e545d8cffbb2d4361d2cd12a53739bbdb8
Taylor Cooke <taylorcooke@gmail.com>
2019-05-31 15:00:29 -0400

    base and github page working

--- core/views.py
+++ core/views.py
@@ -3,12 +3,7 @@ import requests
 
 # Create your views here.
 def home(request):
-    response = requests.get('http://freegeoip.net/json')
-    geodata = response.json()
-    return render(request, 'core/home.html', {
-        'ip': geodata['ip'],
-        'country': geodata['country_name']
-    })
+    return render(request, 'home.html')
 
 def github(request):
     user = {}
@@ -17,4 +12,4 @@ def github(request):
         url = 'https://api.github.com/users/%s' % username
         response = requests.get(url)
         user = response.json()
-    return render(request, 'core/github.html', {'user': user})
+    return render(request, 'github.html', {'user': user})
