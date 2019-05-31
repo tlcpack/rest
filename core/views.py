1b1ff1209ec18b9e96126172b0af4cc9d4187c8b
Taylor Cooke <taylorcooke@gmail.com>
2019-05-31 14:34:57 -0400

    geo API is old, working on github api

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def home(request):
+    response = requests.get('http://freegeoip.net/json')
+    geodata = response.json()
+    return render(request, 'core/home.html', {
+        'ip': geodata['ip'],
+        'country': geodata['country_name']
+    })
+
+def github(request):
+    user = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = `https://api.github.com/users/${username}`
+        response = requests.get(url)
+        user = response.json()
+    return render(request, 'core/github.html', {'user': user})
