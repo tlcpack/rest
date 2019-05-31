11d7c7cb5b18820d0405f9cab33d71c25c902cc8
Taylor Cooke <taylorcooke@gmail.com>
2019-05-31 14:49:19 -0400

    github page created

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('github/', views.github, name='github'),
 ]
--- core/views.py
+++ core/views.py
@@ -14,7 +14,7 @@ def github(request):
     user = {}
     if 'username' in request.GET:
         username = request.GET['username']
-        url = `https://api.github.com/users/${username}`
+        url = 'https://api.github.com/users/%s' % username
         response = requests.get(url)
         user = response.json()
     return render(request, 'core/github.html', {'user': user})
